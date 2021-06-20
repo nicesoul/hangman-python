9a6c393d338e22e085fedfbdb3a36eccd6112a22
nicesoul <nicesoul.beyourself@gmail.com>
2021-06-20 12:26:25 +0200

    command line options and arguments #1
    temporarily commit to add --help and other options to a program. For the moment -small and -large are implemented

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 #! python3
 # -*- coding: utf-8 -*-
 """ This is a main module for hangman game.
-We define a logic of starting, breaking
-and ending of the game here. or not??? :)
+Does nothing but calls the game. Enjoy!
 """
 # enjoy ))))
-
-# below is the initial way of how I started the game
-# if I run the file, it worked with only 1 line here - import...
-
-# import utils.game as game
-# game.Hangman().start_game() #this line originally was in game.py
-
-# but is the below version better somehow?
-# maybe yes, because we can import, alter methods and
-# then decide how we want to run it
 
 import utils.game as hangman
 
